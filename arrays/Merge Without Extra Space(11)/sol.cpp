6f72ec88579a0a94748ef39eb6a9f785f758b9e3
June-24 <mj21csb0b36@student.nitw.ac.in>
2023-03-07 16:54:35 +0530

    Update sol.cpp

--- arrays/Merge Without Extra Space(11)/sol.cpp
+++ arrays/Merge Without Extra Space(11)/sol.cpp
@@ -1,10 +1,73 @@
 /*
 qn: https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1
+bruteforce: just take an array of sixe m+n then put them in it then sort it
+or instead of sorting use two pointer approch and put each elemt taking m+n complexity
+we can go to (m+n)log(m+n) complexity so we are compromising time for space
+so neeed a differnet algorithm
+
+
+///lmaoooooo i dont know but this code is beautiful
+// no hard stuff just basic code
 */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    
+// } Driver Code Ends
+class Solution
+{
+public:
+    // Function to merge the arrays.
+    void merge(long long arr1[], long long arr2[], int n, int m)
+    {
+        // code here
+        int i = n - 1, j = 0;
+        while (i >= 0 && j < m && arr1[i] > arr2[j])
+            swap(arr1[i--], arr2[j++]);
+        sort(arr1,arr1+n);
+        sort(arr2,arr2+m);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        long long arr1[n], arr2[m];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr1[i];
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> arr2[i];
+        }
+        Solution ob;
+        ob.merge(arr1, arr2, n, m);
+
+        for (int i = 0; i < n; i++)
+            cout << arr1[i] << " ";
+
+        for (int i = 0; i < m; i++)
+            cout << arr2[i] << " ";
+
+        cout << endl;
+    }
+
     return 0;
 }
+
+// } Driver Code Ends
